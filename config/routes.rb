a5d63a08de02bed1f39c3817b92a1e7d9fdba671
Mauro Berlanda <m.berlanda@fit2you.it>
2016-02-09 11:56:56 +0100

    added static servers

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :users do
-    collection do
-      get :new
-    end
-  end
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
