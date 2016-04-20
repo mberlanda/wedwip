81b585aeb4b1f13780ab6562dedf4a8ac073a1e9
Mauro Berlanda <m.berlanda@fit2you.it>
2016-04-20 12:23:30 +0200

    added ajax call to add guests into db

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -3,7 +3,7 @@ class GuestsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-
+    byebug
     begin
       g = JSON.parse(guest_params.fetch("guest_json")).with_indifferent_access
       Guest.create!(name: g[:name], surname: g[:surname], user: current_user)
@@ -32,7 +32,7 @@ class GuestsController < ApplicationController
     end
 
     guests = Guest.where(user: current_user).all
-    
+
     respond_to do |format|
       format.json { render json:{ guest_list: guests.to_json, status: @status }  }
     end
