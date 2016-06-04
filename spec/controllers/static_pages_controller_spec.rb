1200efa56ec60a491242152a6bd6cda09026ce7c
Mauro Berlanda <m.berlanda@fit2you.it>
2016-06-04 13:14:37 +0200

    codacy code style corrected

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe StaticPagesController, type: :controller do
 
   let(:user){ User.create!(email: "foo@bar.com", password: "example01", password_confirmation: "example01") }
 
-    it "should login successfully" do
-      sign_in user
-      get :home
-      expect(response).to have_http_status(:success)
-    end
+  it "should login successfully" do
+    sign_in user
+    get :home
+    expect(response).to have_http_status(:success)
+  end
 
 end
